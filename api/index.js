081b401a70aa71d21078c3a36c690d9f385e5d44
TDrake38 <thomasdrake_38@hotmail.com>
2023-01-17 16:01:53 -0330

    created a database connection for mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.listen(8800, () => {
-    console.log("Backend server is running!");
+  console.log("Backend server is running!");
 });
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => console.log(err));
