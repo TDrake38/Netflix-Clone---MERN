f3e2f2fb8af275f37e72faaeaa421eaf69e0a81e
TDrake38 <thomasdrake_38@hotmail.com>
2023-01-23 09:21:52 -0330

    created state for navbar when scrolled

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,17 @@
 import { ArrowDropDown, Notifications, Search } from "@mui/icons-material"
 import "./navbar.scss"
+import { useState } from "react"
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => window.onscroll = null;
+    };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
             <img
